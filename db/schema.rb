bb93f7a7b4a32d262ce7bfcb8f876694f3cd72e3
Andrew <andrewgovroom@hotmail.com>
2017-04-17 00:26:05 +0000

    Assignment 21: Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416214309) do
+ActiveRecord::Schema.define(version: 20170416070738) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 20170416214309) do
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "topic_id"
-  end
 
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+  end
+  
+    add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
