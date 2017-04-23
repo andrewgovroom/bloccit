fd21d40dccddbdb57756b39aa35cf19f81db1f03
Andrew <andrewgovroom@hotmail.com>
2017-04-23 05:54:28 +0000

    Checkpoint 28 Authorization: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,11 +52,20 @@
  end
  
  
- user = User.first
- user.update_attributes!(
-   email: 'andrewgovroom@hotmail.com', # replace this with your personal email
-   password: 'helloworld'
- )
+  # Create an admin user
+    admin = User.create!(
+     name:     'Admin User',
+     email:    'admin@example.com',
+     password: 'helloworld',
+     role:     'admin'
+    )
+   
+   # Create a member
+    member = User.create!(
+     name:     'Member User',
+     email:    'member@example.com',
+     password: 'helloworld'
+    )
  
  
  puts "Seed finished"
